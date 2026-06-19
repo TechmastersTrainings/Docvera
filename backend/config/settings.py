e0d98fe608a66848bf66d9093e89b9f6934555cd
Sachin Anil <sachin@Sachins-MacBook-Air.local>
2026-06-19 23:12:51 +0530

    Switch to PostgreSQL

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -111,24 +111,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # DATABASE
 # =========================================================
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+# =========================================================
+# DATABASE
+# =========================================================
 
-if DATABASE_URL:
-    DATABASES = {
-        "default": dj_database_url.parse(
-            DATABASE_URL,
-            conn_max_age=600,
-            ssl_require=True
-        )
-    }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
-
+DATABASES = {
+    "default": dj_database_url.parse(
+        os.environ["DATABASE_URL"],
+        conn_max_age=600,
+        ssl_require=False
+    )
+}
 # =========================================================
 # CUSTOM USER MODEL
 # =========================================================
