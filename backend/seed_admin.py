97723d11784f120d8f346f213d171a620f9ee10a
Sachin Anil <sachin@Sachins-MacBook-Air.local>
2026-06-21 23:40:12 +0530

    Update automated admin credentials

--- backend/seed_admin.py
+++ backend/seed_admin.py
@@ -7,8 +7,8 @@ django.setup()
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
-email = "admin@docvera.com"
-password = "AdminPassword123!"
+email = "Techmasterstrainings@gmail.com"
+password = "Fri1025Feb@2023"
 
 if not User.objects.filter(email=email).exists():
     User.objects.create_superuser(email=email, password=password)
